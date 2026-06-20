bf2a431e4277ff2ab217f9bd662bc805943f11f2
okano-tomoyuki <okano.development@gmail.com>
2026-06-20 15:26:44 +0900

    Ubuntu上でのビルドエラーを解消する。

--- cmake/FindTCL.cmake
+++ cmake/FindTCL.cmake
@@ -1,5 +1,10 @@
-find_path(TCL_INCLUDE_PATH tcl.h)
-find_library(TCL_LIBRARY NAMES tcl)
+find_path(TCL_INCLUDE_PATH
+    NAMES tcl.h
+    PATH_SUFFIXES tcl8.6 tcl8.5 tcl8.4
+)
+find_library(TCL_LIBRARY
+    NAMES tcl tcl9.0 tcl90 tcl8.7 tcl87 tcl8.6 tcl86 tcl8.5 tcl85 tcl8.4 tcl84
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(TCL DEFAULT_MSG TCL_LIBRARY TCL_INCLUDE_PATH)
--- cmake/FindTk.cmake
+++ cmake/FindTk.cmake
@@ -1,5 +1,10 @@
-find_path(Tk_INCLUDE_PATH tk.h)
-find_library(Tk_LIBRARY NAMES tk)
+find_path(Tk_INCLUDE_PATH
+    NAMES tk.h
+    PATH_SUFFIXES tcl8.6 tcl8.5 tcl8.4
+)
+find_library(Tk_LIBRARY
+    NAMES tk tk9.0 tk90 tk8.7 tk87 tk8.6 tk86 tk8.5 tk85 tk8.4 tk84
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Tk DEFAULT_MSG Tk_LIBRARY Tk_INCLUDE_PATH)
